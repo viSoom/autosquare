5e5c795769d40829ccf3ecdfc088382be26aeb93
Marco <nogard20@gmail.com>
2015-11-26 18:42:41 +0100

    added scope to protect $ alias

--- easy/js/autosquare.js
+++ easy/js/autosquare.js
@@ -4,14 +4,16 @@ jQuery AutoSquare v.1.0
 @Author: Eduardo Allegrini - http://www.eduardoallegrini.com/
 @Copyright: 07/08/2015
 ************************/
-$.fn.autoSquare = function() {
-	var thisElement = this;
-	thisElement.each(function(){
-		var thisOnly = $(this);
-		var thisProportion = $.trim(thisOnly.attr('data-proportion'));
-		if(!thisProportion || !$.isNumeric(thisProportion) || thisProportion < 0 || thisProportion > 6) {
-			thisProportion = 1;
-		}
-		thisOnly.height(thisOnly.width()*thisProportion);
-	});
-}
+(function ($) {
+    $.fn.autoSquare = function() {
+		var thisElement = this;
+		thisElement.each(function(){
+			var thisOnly = $(this);
+			var thisProportion = $.trim(thisOnly.attr('data-proportion'));
+			if(!thisProportion || !$.isNumeric(thisProportion) || thisProportion < 0 || thisProportion > 6) {
+				thisProportion = 1;
+			}
+			thisOnly.height(thisOnly.width()*thisProportion);
+		});
+	}
+}(jQuery));
